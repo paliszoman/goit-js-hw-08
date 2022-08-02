903e51befe68a6cda64b4b5cf100640f78c0c453
paliszoman <kamilkrolik@hotmail.com>
2022-08-02 21:06:46 +0200

    new1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery");
+const galleryImg = galleryItems.map(item=>`
+  <a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt=${item.description} title=${item.description}/>
+</a>`).join(" ")
+gallery.innerHTML = galleryImg
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250});
+
