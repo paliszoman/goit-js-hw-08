a8a6f9069323555d0fd924721c3e152004f246be
paliszoman <kamilkrolik@hotmail.com>
2022-08-02 23:30:04 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import Player from '@vimeo/player';
-import _ from 'lodash.throttle'
+import { throttle } from 'lodash.throttle'
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 player.on('timeupdate',
-    _.throttle(({seconds }) => { localStorage.setItem("videoplayer-current-time", seconds) }));
+   throttle(({seconds }) => { localStorage.setItem("videoplayer-current-time", seconds) }));
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
