929ffca1a804e47dd6ae33ba4a4c418deda60294
paliszoman <kamilkrolik@hotmail.com>
2022-08-07 17:53:23 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,19 @@
-import { throttle } from 'lodash';
+import {throttle} from 'lodash';
+
+const form = document.querySelector('.feedback-form');
+const mail = document.querySelector('input');
+const textMessage = document.querySelector('textarea')
 
 let submitForm = (event) => { 
     event.preventDefault();
     let { elements: { email, message } } = event.currentTarget;
     if (email.value == '' || message.value == '') { alert("Please fill in all the fields!") }
     else {
-        localStorage.removeItem("feedback-form-state");
+        
         console.log(`Send mail to: ${email.value}, message is: ${message.value}`);
         event.currentTarget.reset();
+        localStorage.clear();
     }
-   
 }
 
 let formData = (event) => { 
@@ -24,24 +28,19 @@ let formData = (event) => {
 
 let loadStorage = () => { 
     let savedData = localStorage.getItem("feedback-form-state");
-    savedData = JSON.parse(savedData);
+    let parsedData = JSON.parse(savedData);
     
-    if (savedData.emailData !== '' || savedData.messageData !== '') {
+    if (savedData !== null) {
         try {
-            form.elements.email.value = savedData.emailData;
-            form.elements.message.innerHTML = savedData.messageData;
+            mail.value = parsedData.emailData;
+            textMessage.innerHTML = parsedData.messageData;
         } catch (error) {
-            console.error("Get state error: ", error.message);
+            console.error("Get state error: ", error.message)
+            ;
         }
     }
-  
 };
-
-
-let form = document.querySelector('.feedback-form')
-let throttled = throttle(function () { loadStorage() }, 500);
-
 form.addEventListener('submit', submitForm);
-form.addEventListener('input',  formData );
-throttled()
+form.addEventListener('input',  throttle(formData , 500) );
+loadStorage()
 
