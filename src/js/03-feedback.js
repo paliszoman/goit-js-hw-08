1d1f7e3bb22a9a4c6df23873b559860705c75ce0
paliszoman <kamilkrolik@hotmail.com>
2022-08-07 18:17:06 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,13 +3,14 @@ import {throttle} from 'lodash';
 const form = document.querySelector('.feedback-form');
 const mail = document.querySelector('input');
 const textMessage = document.querySelector('textarea')
+const savedData = localStorage.getItem("feedback-form-state");
+const parsedData = JSON.parse(savedData);
 
 let submitForm = (event) => { 
     event.preventDefault();
     let { elements: { email, message } } = event.currentTarget;
     if (email.value == '' || message.value == '') { alert("Please fill in all the fields!") }
     else {
-        
         console.log(`Send mail to: ${email.value}, message is: ${message.value}`);
         event.currentTarget.reset();
         localStorage.clear();
@@ -17,7 +18,6 @@ let submitForm = (event) => {
 }
 
 let formData = (event) => { 
-    event.preventDefault();
     let { elements: { email, message } } = event.currentTarget;
     let input = {
         emailData: email.value,
@@ -27,20 +27,16 @@ let formData = (event) => {
 }
 
 let loadStorage = () => { 
-    let savedData = localStorage.getItem("feedback-form-state");
-    let parsedData = JSON.parse(savedData);
-    
     if (savedData !== null) {
         try {
             mail.value = parsedData.emailData;
             textMessage.innerHTML = parsedData.messageData;
         } catch (error) {
-            console.error("Get state error: ", error.message)
-            ;
+            console.error("Get state error: ", error.message);
         }
     }
 };
+form.addEventListener('input',  throttle(formData , 500));
 form.addEventListener('submit', submitForm);
-form.addEventListener('input',  throttle(formData , 500) );
 loadStorage()
 
